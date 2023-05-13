7a9549c56e2505fa9d4bf6e68273f645b13c8f76
trungquandev <trungquandev.official@gmail.com>
2023-05-13 15:24:46 +0700

    Update Eslint files

---
+++ 3-sample-eslint-config-for-nodejs-project/.eslintrc.cjs
@@ -0,0 +1,32 @@
+// Updated by trungquandev.com's author on May 13 2023
+// Sample Eslint config for Node.js project
+module.exports = {
+  env: { browser: true, es2020: true, node: true },
+  extends: [ 'eslint:recommended' ],
+  parser: '@babel/eslint-parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module', allowImportExportEverywhere: true },
+  settings: { react: { version: '18.2' } },
+  plugins: [],
+  rules: {
+    'no-console': 1,
+    'no-lonely-if': 1,
+    'no-unused-vars': 1,
+    'no-trailing-spaces': 1,
+    'no-multi-spaces': 1,
+    'no-multiple-empty-lines': 1,
+    'space-before-blocks': ['error', 'always'],
+    'object-curly-spacing': [1, 'always'],
+    'indent': ['warn', 2],
+    'semi': [1, 'never'],
+    'quotes': ['error', 'single'],
+
+    'array-bracket-spacing': 1,
+    'linebreak-style': 0,
+    'no-unexpected-multiline': 'warn',
+
+    'keyword-spacing': 1,
+    'comma-dangle': 1,
+    'comma-spacing': 1,
+    'arrow-spacing': 1
+  }
+}
