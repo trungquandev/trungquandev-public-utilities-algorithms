8dd221b2e062189f75076f22726cbe246220d00d
trungquandev <trungquandev.official@gmail.com>
2023-06-01 01:02:12 +0700

    Vite absolute relative import

---
+++ 9-vite-absolute-relative-import/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // base: './'
+  resolve: {
+    alias: [
+      { find: '~', replacement: '/src' }
+    ]
+  }
+})
