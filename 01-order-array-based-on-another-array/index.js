1ee45cb2ff752124f0b43f5a6044c85cef00b59d
trungquandev <trungquandev.official@gmail.com>
2023-06-29 03:32:38 +0700

    Fix typo

--- 01-order-array-based-on-another-array/index.js
+++ 01-order-array-based-on-another-array/index.js
@@ -11,7 +11,7 @@
  * @return new Ordered Array
  *
  * For Vietnamese with love :D
- * Xác định các phần tử trong array gốc ban đầu (originalArray) xem nó nằm ở đâu trong array thứ 2 (orderArray) (là array mà mình dùng để sắp xếp) bằng cách tìm index (indexOf) rồi sẽ sắp xếp theo index đó bằng hàm sort của Javascript. 
+ * Xác định các phần tử trong array gốc ban đầu (originalArray) xem nó nằm ở đâu trong array thứ 2 (orderArray) (là array mà mình dùng để sắp xếp) bằng cách tìm index (indexOf) rồi sẽ sắp xếp theo index đó bằng hàm sort của Javascript.
  */
 
 const mapOrder = (originalArray, orderArray, key) => {
