e6f85857c0d00153a272d5c8fb85906860e0c068
trungquandev <trungquandev.official@gmail.com>
2023-09-27 18:56:36 +0700

    13. String to Slug

--- 13-string-to-slug/index.js
+++ 13-string-to-slug/index.js
@@ -8,7 +8,7 @@
  */
 const slugify = (val) => {
   if (!val) return ''
-  return val
+  return String(val)
     .normalize('NFKD') // split accented characters into their base characters and diacritical marks
     .replace(/[\u0300-\u036f]/g, '') // remove all the accents, which happen to be all in the \u03xx UNICODE block.
     .trim() // trim leading or trailing whitespace
