5a835f7d2b6e85e141d5f9bda4a39853d74e23ba
trungquandev <trungquandev.official@gmail.com>
2023-10-08 17:19:33 +0700

    14 - add model schema

--- 14-trello-mongodb-schemas/cardModel.js
+++ 14-trello-mongodb-schemas/cardModel.js
@@ -8,7 +8,7 @@ import Joi from 'joi'
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
 
 // Define Collection (name & schema)
-const CARD_COLLECTION_NAME = 'boards'
+const CARD_COLLECTION_NAME = 'cards'
 const CARD_COLLECTION_SCHEMA = Joi.object({
   boardId: Joi.string().required().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
   columnId: Joi.string().required().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
--- 14-trello-mongodb-schemas/columnModel.js
+++ 14-trello-mongodb-schemas/columnModel.js
@@ -8,7 +8,7 @@ import Joi from 'joi'
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
 
 // Define Collection (name & schema)
-const COLUMN_COLLECTION_NAME = 'boards'
+const COLUMN_COLLECTION_NAME = 'columns'
 const COLUMN_COLLECTION_SCHEMA = Joi.object({
   boardId: Joi.string().required().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
   title: Joi.string().required().min(3).max(50).trim().strict(),
