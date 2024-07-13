b40d0bba6740e4edde94d210c3611b15f08cee44
trungquandev <quan.do@cohost.ai>
2024-07-14 02:52:06 +0700

    Push code.

--- 17-base-auth0-sso-react-node/node-server-sso-trungquandev/onExecutePostLoginExample.js
+++ 17-base-auth0-sso-react-node/node-server-sso-trungquandev/onExecutePostLoginExample.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 // Author: TrungQuanDev | https://youtube.com/@trungquandev
 /**
  * Đây là một ví dụ về cách sử dụng onExecutePostLogin Flow của Auth0 để gọi sang api backend của chúng ta
@@ -18,22 +19,22 @@ exports.onExecutePostLogin = async (event, api) => {
     const response = await axios.post(`https://${AUTH0_DOMAIN}/oauth/token`, {
       client_id: AUTH0_CLIENT_ID,
       client_secret: AUTH0_CLIENT_SECRET,
-      audience: API_ENDPOINT,
+      audience: RENDER_API_ENDPOINT,
       grant_type: 'client_credentials'
     })
     return response.data.access_token
   }
   const accessToken = await getAccessToken()
-  console.log("accessToken: ", accessToken)
-  
+  console.log('accessToken: ', accessToken)
+
   try {
     // THông tin user sau khi login qua Auth0 sẽ được lấy từ event.user
-    console.log("event.user: ", event.user)
+    console.log('event.user: ', event.user)
     // Gọi tới API trên server backend của chúng ta để lưu user vào Database
-    await axios.post(`${API_ENDPOINT}/api-v1/users/private/hook/login`, event.user, {
+    await axios.post(`${RENDER_API_ENDPOINT}/api-v1/users/private/hook/login`, event.user, {
       headers: {
         'Content-Type': 'application/json',
-        "Authorization": `Bearer ${accessToken}`
+        'Authorization': `Bearer ${accessToken}`
       }
     })
   } catch (error) {
