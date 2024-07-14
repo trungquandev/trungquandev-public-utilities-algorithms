4aa71149d58bddfc9564c5e22adcc6d6d81d0983
trungquandev <quan.do@cohost.ai>
2024-07-15 00:13:38 +0700

    Push code.

--- 17-base-auth0-sso-react-node/react-sso-trungquandev/src/utils/customAxios.js
+++ 17-base-auth0-sso-react-node/react-sso-trungquandev/src/utils/customAxios.js
@@ -5,4 +5,19 @@ import axios from "axios"
 // Cách custom axios instance với interceptors tự động lấy access token cũng như refresh token của Auth0 và và gán vào header trước khi gọi api.
 const customAxiosInstance = axios.create()
 
+// // Tương tự cách làm của redux ở đây:
+// // https://redux.js.org/faq/code-structure#how-can-i-use-the-redux-store-in-non-component-files
+// let getAccessTokenSilently
+// export const injectFn = _getAccessTokenSilently => {
+//   getAccessTokenSilently = _getAccessTokenSilently
+// }
+
+// // Request interceptor
+// customAxiosInstance.interceptors.request.use(async (config) => {
+//   const accessToken = await getAccessTokenSilently()
+//   console.log('accessToken: ', accessToken)
+//   config.headers.Authorization = `Bearer ${accessToken}`
+//   return config
+// })
+
 export default customAxiosInstance
