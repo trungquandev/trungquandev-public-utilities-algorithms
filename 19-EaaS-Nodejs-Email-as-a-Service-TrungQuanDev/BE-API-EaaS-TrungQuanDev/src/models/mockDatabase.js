675297c0d214909082d2f5ebe2706063f6ec682f
trungquandev <trungquandev.official@gmail.com>
2025-05-31 15:40:11 +0700

    Push code base 19: EaaS, Email as a Service, NodeJS, TrungQuanDev

--- 19-EaaS-Nodejs-Email-as-a-Service-TrungQuanDev/BE-API-EaaS-TrungQuanDev/src/models/mockDatabase.js
+++ 19-EaaS-Nodejs-Email-as-a-Service-TrungQuanDev/BE-API-EaaS-TrungQuanDev/src/models/mockDatabase.js
@@ -9,5 +9,6 @@
 export const MOCK_USER = {
   ID: 'trungquandev-sample-id-12345678',
   EMAIL: 'trungquandev.official@gmail.com',
-  PASSWORD: 'trungquandev@123'
+  PASSWORD: 'trungquandev@123',
+  USERNAME: 'TrungQuanDev - Một Lập Trình Viên'
 }
