f898f358e7fba332d41deef7fcaa222a539ff8bf
quandt <quandt@bfast.dev>
2025-12-23 00:03:39 +0700

    Push code

--- 22-Unit-Test-NodeJS-TypeScript-TrungQuanDev/Unit-Test-Nodejs-TypeScript-TrungQuanDev/src/core/http/errorHandler.ts
+++ 22-Unit-Test-NodeJS-TypeScript-TrungQuanDev/Unit-Test-Nodejs-TypeScript-TrungQuanDev/src/core/http/errorHandler.ts
@@ -1,6 +1,7 @@
 /** Author: @trungquandev (TrungQuanDev - Một Lập Trình Viên) - https://youtube.com/@trungquandev */
 
 import { Request, Response, NextFunction } from 'express'
+import { StatusCodes } from 'http-status-codes'
 
 import { env } from '~/config/env'
 import { ApiError } from '~/core/http/ApiError'
@@ -9,14 +10,14 @@ import { ApiError } from '~/core/http/ApiError'
  * Quan Trọng: Nơi xử lý lỗi tập trung cho toàn bộ express app, gọi: app.use(errorHandler)
  */
 export function errorHandler(err: unknown, _req: Request, res: Response, _next: NextFunction) {
-  // Custom Api Error
-  if (err instanceof ApiError) {
-    return res.status(err.statusCode).json({ message: err.message, details: err.details })
-  }
   // Log chi tiết khi dev
   if (env.NODE_ENV === 'development') {
     console.error('[Unhandled Error]', err)
   }
+  // Custom Api Error
+  if (err instanceof ApiError) {
+    return res.status(err.statusCode).json({ message: err.message, details: err.details })
+  }
   // Trả lỗi chung nếu chưa handle
-  return res.status(500).json({ message: 'Internal Server Error' })
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Internal Server Error' })
 }
